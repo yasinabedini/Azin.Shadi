3aa31968fb324be9014393e06514524046f73a5b
yasin abedini <yasinabedini1384@gmail.com>
2023-03-10 22:26:31 +0330

    fix - delete Mnagecontroller,It's Action And Move ManageController's  Action to AdminController & Fix admin Login Action

--- Azin.Shadi/Controllers/AdminController.cs
+++ Azin.Shadi/Controllers/AdminController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Azin.Shadi.Models;
+using Azin.Shadi.Models.ViewModels;
 
 namespace Azin.Shadi.Controllers
 {
@@ -17,6 +18,35 @@ namespace Azin.Shadi.Controllers
             return View(adminList);
         }
 
-        
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login([Bind(Include = "Mobile,Password")] AdminLoginViewModel admin)
+        {
+            if (ModelState.IsValid)
+            {
+                if (!db.Admins.Any(t => t.Mobile == admin.Mobile))
+                {
+                    ModelState.AddModelError("Mobile", "هیچ ادمینی با این شماره موبایل ثبت نشده است");
+                    return View();
+                }
+                var logedAdmin = db.Admins.First(t => t.Mobile == admin.Mobile);
+                if (logedAdmin.Password != admin.Password)
+                {
+                    ModelState.AddModelError("Password", "رمز عبور وارد نشده نادرست است!");
+                    return View();
+                }
+                return View("DashBoard");
+            }
+
+            return View();
+        }
+
+
     }
 }
---
+++ Azin.Shadi/Models/ViewModels/AdminLoginViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Azin.Shadi.Models.ViewModels
+{
+    public class AdminLoginViewModel
+    {
+        [Display(Name ="موبایل")]
+        [Required(ErrorMessage =" {0} را وارد کنید")]
+        [Phone]
+        public string Mobile { get; set; }
+
+        [Display(Name = "پسورد")]
+        [Required(ErrorMessage = " {0} را وارد کنید")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+    }
+}
