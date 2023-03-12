26c1dab879024fb917cbfc53b203aeafb19fe0f0
yasin abedini <yasinabedini1384@gmail.com>
2023-03-12 23:32:49 +0330

    feat - Add AutoMapper To Project

---
+++ Azin.Shadi/App_Start/AutoMapperConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using Azin.Shadi.Models;
+using Azin.Shadi.Models.ViewModels;
+
+namespace Azin.Shadi.App_Start
+{
+    public class AutoMapperConfig
+    {
+        public static IMapper mapper;
+
+        public static void Configuration()
+        {
+            MapperConfiguration configuration = new MapperConfiguration(t =>
+            {
+                t.CreateMap<Admin, RegisterAdminViewModel>();
+            });
+            mapper = configuration.CreateMapper();
+        }
+
+}
+}
--- Azin.Shadi/Global.asax.cs
+++ Azin.Shadi/Global.asax.cs
@@ -6,6 +6,7 @@ using System.Threading;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using Azin.Shadi.App_Start;
 
 namespace Azin.Shadi
 {
@@ -15,6 +16,7 @@ namespace Azin.Shadi
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            AutoMapperConfig.Configuration();
         }
 
         protected void Application_BeginRequest(object sender, EventArgs e)
@@ -26,6 +28,6 @@ namespace Azin.Shadi
             persianCulture.DateTimeFormat.PMDesignator = "ب.ظ";
             Thread.CurrentThread.CurrentCulture = persianCulture;
             Thread.CurrentThread.CurrentUICulture = persianCulture;
-        }
+        }        
     }
 }
