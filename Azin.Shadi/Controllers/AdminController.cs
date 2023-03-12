787bbde0c28daffee3d7a545313d4cf3fcd99cfd
yasin abedini <yasinabedini1384@gmail.com>
2023-03-12 23:35:02 +0330

    feat - complate Login Action, Add Dashboard, Register Action To AdminController

--- Azin.Shadi/Controllers/AdminController.cs
+++ Azin.Shadi/Controllers/AdminController.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using Azin.Shadi.Models;
 using Azin.Shadi.Models.ViewModels;
+using System.Net;
+using System.IO;
 
 namespace Azin.Shadi.Controllers
 {
@@ -12,13 +14,61 @@ namespace Azin.Shadi.Controllers
     {
         AzinShadiDbContext db = new AzinShadiDbContext();
 
+
+        #region Dashboard
+        [HttpGet]
+        public ActionResult Dashboard(Admin admin)
+        {
+            if (admin.Mobile == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            return View();
+        }
+        #endregion
+
+        #region Index
         public ActionResult Index()
         {
             var adminList = db.Admins;
             return View(adminList);
         }
+        #endregion
+
+        #region Create
+
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register([Bind(Include = "")] RegisterAdminViewModel admin, HttpPostedFileBase profileImage)
+        {
+            if (ModelState.IsValid)
+            {
+                string imageName = "defaultprofile.png";
+                if (profileImage != null)
+                {
+                    if (profileImage.ContentType != "image/jpeg" && profileImage.ContentType != "image/png")
+                    {
+                        ModelState.AddModelError("","فقط عکس های با فرمت Png و jpeg قابل قبول است. ");
+                    }
 
 
+                    imageName = Guid.NewGuid().ToString().Replace("-", "") + Path.GetExtension(profileImage.FileName);
+                }
+
+
+            }
+
+            return View();
+        }
+
+        #endregion
+
+        #region Login
         [HttpGet]
         public ActionResult Login()
         {
@@ -38,7 +88,7 @@ namespace Azin.Shadi.Controllers
                 var logedAdmin = db.Admins.First(t => t.Mobile == admin.Mobile);
                 if (logedAdmin.Password == admin.Password)
                 {
-                    return View("DashBoard");
+                    return RedirectToAction("DashBoard", logedAdmin);
                 }
                 ModelState.AddModelError("Password", "رمز عبور وارد نشده نادرست است!");
                 return View();
@@ -46,7 +96,7 @@ namespace Azin.Shadi.Controllers
 
             return View();
         }
-
+        #endregion
 
     }
 }
---
+++ Azin.Shadi/Models/ViewModels/RegisterAdminViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Azin.Shadi.Models.ViewModels
+{
+    public class RegisterAdminViewModel
+    {
+        [Display(Name = "نام")]
+        [Required(ErrorMessage = "{0} را پر کنید")]
+        public string Name { get; set; }
+
+        [Display(Name = "نام خانوادگی")]
+        [Required(ErrorMessage = "{0} را پر کنید")]
+        public string Family { get; set; }
+
+        [Display(Name = "نام کاربری")]
+        public string Username { get; set; }
+
+        [Display(Name = "رمز عبور")]
+        [Required(ErrorMessage = "{0} را پر کنید")]
+        public string Password { get; set; }
+
+        [Display(Name="تکرار رمز عبور")]
+        [Compare("Password")]
+        [Required]
+        public string RepeatPassword { get; set; }
+
+        [Display(Name = "ایمیل")]
+        [EmailAddress(ErrorMessage = "ایمیل خود را به درستی وارد کنید!")]
+        public string Eamil { get; set; }
+
+        [Display(Name = "موبایل")]
+        [Required(ErrorMessage = "{0} را پر کنید")]
+        [RegularExpression("(09){9}[0-9]", ErrorMessage = "شماره موبایل را درست وارد کنید")]
+        public string Mobile { get; set; }
+
+        [Display(Name = "دپارتمان فعالیت")]
+        [Required(ErrorMessage = "{0} را پر کنید")]
+        public string Department { get; set; }
+    }
+}
